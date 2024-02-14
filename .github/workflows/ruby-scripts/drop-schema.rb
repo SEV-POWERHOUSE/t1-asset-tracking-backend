5ce66f1ac66ab8f46e7d17bc1274d0801df7f2eb
Zfike <97480418+Zfike@users.noreply.github.com>
2024-02-14 10:19:35 -0600

    Revert "Attempt to fix AWS socket use"

--- .github/workflows/ruby-scripts/drop-schema.rb
+++ .github/workflows/ruby-scripts/drop-schema.rb
@@ -6,19 +6,14 @@ Dotenv.load(File.expand_path('../../../../.env', __FILE__))
 # Function to insert data into the MySQL database
 def drop_schema
   begin
-    # Build the connection options
-    connection_options = {
-      host: ENV['DB_HOST'],
-      username: ENV['DB_USER'],
-      password: ENV['DB_PW'],
-      database: ENV['DB_NAME']
-    }
-    
-    # Add the socket option only if DB_SOCKET is set
-    connection_options[:socket] = ENV['DB_SOCKET'] if ENV['DB_SOCKET']
-
     # Connect to MySQL
-    client = Mysql2::Client.new(connection_options)
+    client = Mysql2::Client.new(
+      :host => ENV['DB_HOST'],
+      :username => ENV['DB_USER'],
+      :password => ENV['DB_PW'],
+      :database => ENV['DB_NAME'],
+      :socket => ENV['DB_SOCKET']
+    )
 
     # drop schema if exists
     client.query("DROP DATABASE IF EXISTS `asset-t1`")
