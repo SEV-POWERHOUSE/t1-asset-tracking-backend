c283762a758f7d17cdb8b84e720936922f0b2457
Zfike <97480418+Zfike@users.noreply.github.com>
2024-02-23 14:26:51 -0600

    Revert "Import csv data zf"

--- app/routes/assetCategory.routes.js
+++ app/routes/assetCategory.routes.js
@@ -6,9 +6,6 @@ module.exports = (app) => {
   // Create a new AssetCategory
   router.post("/", [authenticate], assetCategory.createAssetCategory);
 
-  // Bulk create from csv
-  router.post("/file", [authenticate], assetCategory.bulkCreateAssetCategory)
-
   // Retrieve all AssetCategories
   router.get("/", [authenticate], assetCategory.getAllAssetCategories);
 
