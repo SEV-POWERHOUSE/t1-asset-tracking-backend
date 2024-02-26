099a4145c544fd1255d9512a89bbc8826b5a0e43
Jaxen McRay <jxnmcray@gmail.com>
2024-02-26 17:15:48 -0600

    Improved and expanded asset and user test data

--- app/scripts/assetCategoryInit.js
+++ app/scripts/assetCategoryInit.js
@@ -2,23 +2,34 @@ const db = require("../models");
 const assetCategory = db.assetCategory;
 
 async function initializeAssetCategory() {
-  try {
-    await Promise.all([
-      await assetCategory.upsert({
-        categoryName: "Devices",
-        desc: "This category encompasses all portable electronic equipment that can be used for work or personal tasks. Examples include smartphones, laptops, tablets, and peripherals like keyboards and mice.",
-      }),
-      await assetCategory.upsert({
-        categoryName: "Infrastructure",
-        desc: "This category includes assets that are part of the underlying framework of technology systems. It covers network equipment (routers, switches), servers, storage devices, and backup systems.",
-      }),
-      await assetCategory.upsert({
-        categoryName: "Facility",
-        desc: "Assets under this category involve physical or tangible assets used in the maintenance and operations of a physical space. This includes security systems, HVAC systems, lighting, and furniture.",
-      }),
-    ]);
+  const categories = [
+    {
+      categoryName: "Devices",
+      desc: "This category encompasses all portable electronic equipment that can be used for work or personal tasks. Examples include smartphones, laptops, tablets, and peripherals like keyboards and mice.",
+    },
+    {
+      categoryName: "Facility",
+      desc: "This category includes assets that are part of the college's buildings and grounds. Examples include HVAC systems, projectors, furniture, and security systems.",
+    },
+    {
+      categoryName: "Infrastructure",
+      desc: "This category comprises assets that support the college's operations but are not directly used by individuals. Examples include networking equipment, servers, and backup power supplies.",
+    },
+    {
+      categoryName: "Computing Hardware",
+      desc: "This category includes desktop computers, servers, printers, and other peripherals not classified as portable devices. It focuses on equipment that supports the IT infrastructure and user computing needs.",
+    },
+    {
+      categoryName: "Audiovisual Equipment",
+      desc: "Comprises equipment used for the presentation and dissemination of audio and visual content. Examples include speakers, microphones, cameras, and video conferencing systems.",
+    },
+  ];
 
-    console.log("Asset Category initialized successfully");
+  try {
+    await Promise.all(
+      categories.map((category) => assetCategory.upsert(category))
+    );
+    console.log("Asset Categories initialized successfully");
   } catch (error) {
     console.log("Initialization failed:", error);
   }
--- app/scripts/personInit.js
+++ app/scripts/personInit.js
@@ -1,140 +1,122 @@
 const db = require("../models");
-const user = db.user;
+const Person = db.person;
 
-async function initializeUser() {
+async function initializePersons() {
   try {
     await Promise.all([
-      user.upsert({
+      Person.upsert({
         fName: "Jaxen",
         lName: "McRay",
         email: "jaxen.mcray@eagles.oc.edu",
-        userRoleId: 1,
-        devPermission: true,
+        idNumber: "1000001",
       }),
-      user.upsert({
+      Person.upsert({
         fName: "Zane",
         lName: "Fike",
         email: "z.fike@eagles.oc.edu",
-        userRoleId: 1,
-        devPermission: true,
+        idNumber: "1000002",
       }),
-      user.upsert({
+      Person.upsert({
         fName: "Solomon",
         lName: "Granger",
         email: "solomon.granger@eagles.oc.edu",
-        userRoleId: 1,
-        devPermission: true,
+        idNumber: "1000003",
       }),
-      user.upsert({
+      Person.upsert({
         fName: "Justin",
         lName: "Davis",
         email: "justin.davis@eagles.oc.edu",
-        userRoleId: 1,
-        devPermission: true,
+        idNumber: "1000004",
       }),
-      user.upsert({
+      Person.upsert({
         fName: "Emily",
         lName: "Wong",
         email: "emily.wong@eagles.oc.edu",
-        userRoleId: 5,
-        devPermission: false,
+        idNumber: "1000005",
       }),
-      user.upsert({
+      Person.upsert({
         fName: "Marcus",
         lName: "Bennett",
         email: "marcus.bennett@eagles.oc.edu",
-        userRoleId: 6,
-        devPermission: false,
+        idNumber: "1000006",
       }),
-      user.upsert({
+      Person.upsert({
         fName: "Arthur",
         lName: "Morgan",
         email: "arthur.morgan@eagles.oc.edu",
-        userRoleId: 3,
-        devPermission: false,
+        idNumber: "1000007",
       }),
-      user.upsert({
+      Person.upsert({
         fName: "Lucas",
         lName: "Morris",
         email: "lucas.morris@eagles.oc.edu",
-        userRoleId: 4,
-        devPermission: false,
+        idNumber: "1000008",
       }),
-      user.upsert({
+      Person.upsert({
         fName: "Sophia",
         lName: "Garcia",
         email: "sophia.garcia@eagles.oc.edu",
-        userRoleId: 7,
-        devPermission: false,
+        idNumber: "1000009",
       }),
-      user.upsert({
+      Person.upsert({
         fName: "Ethan",
         lName: "Lee",
         email: "ethan.lee@eagles.oc.edu",
-        userRoleId: 8,
-        devPermission: false,
+        idNumber: "1000010",
       }),
-      user.upsert({
+      Person.upsert({
         fName: "Alex",
         lName: "Johnson",
         email: "alex.johnson@eagles.oc.edu",
-        userRoleId: 2,
-        devPermission: false,
+        idNumber: "1000011",
       }),
-      user.upsert({
+      Person.upsert({
         fName: "Isabella",
         lName: "Martinez",
         email: "isabella.martinez@eagles.oc.edu",
-        userRoleId: 1,
-        devPermission: false,
+        idNumber: "1000012",
       }),
-      user.upsert({
+      Person.upsert({
         fName: "Elijah",
         lName: "Hernandez",
         email: "elijah.hernandez@eagles.oc.edu",
-        userRoleId: 5,
-        devPermission: false,
+        idNumber: "1000013",
       }),
-      user.upsert({
+      Person.upsert({
         fName: "Zoe",
         lName: "Robinson",
         email: "zoe.robinson@eagles.oc.edu",
-        userRoleId: 6,
-        devPermission: false,
+        idNumber: "1000014",
       }),
-      user.upsert({
+      Person.upsert({
         fName: "Adrian",
         lName: "Clark",
         email: "adrian.clark@eagles.oc.edu",
-        userRoleId: 3,
-        devPermission: false,
+        idNumber: "1000015",
       }),
-      user.upsert({
+      Person.upsert({
         fName: "Samantha",
         lName: "Lewis",
         email: "samantha.lewis@eagles.oc.edu",
-        userRoleId: 4,
-        devPermission: false,
+        idNumber: "1000016",
       }),
-      user.upsert({
+      Person.upsert({
         fName: "Ian",
         lName: "Walker",
         email: "ian.walker@eagles.oc.edu",
-        userRoleId: 7,
-        devPermission: false,
+        idNumber: "1000017",
       }),
-      user.upsert({
+      Person.upsert({
         fName: "Julia",
         lName: "Perez",
         email: "julia.perez@eagles.oc.edu",
-        userRoleId: 8,
-        devPermission: false,
+        idNumber: "1000018",
       }),
     ]);
-    console.log("Users initialized successfully");
+    console.log("Persons initialized successfully");
   } catch (error) {
     console.log("Initialization failed:", error);
   }
 }
 
-module.exports = { initializeUser };
+module.exports = { initializePersons };
