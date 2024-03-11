51126f17c7593ceadf40c1092846fa9d44819951
Solomon <solomon.granger@eagles.oc.edu>
2024-03-11 13:49:39 -0500

    added stuff

--- app/scripts/userRoleInit.js
+++ app/scripts/userRoleInit.js
@@ -6,14 +6,62 @@ async function initializeUserRole() {
     try {
 
         await Promise.all([
-            await userRole.upsert({ name: 'Admin' }),
-            await userRole.upsert({ name: 'Unassigned' }),
-            await userRole.upsert({ name: 'Maintenance Manager' }),
-            await userRole.upsert({ name: 'Maintenance Worker' }),
-            await userRole.upsert({ name: 'IT Manager' }),
-            await userRole.upsert({ name: 'IT Worker' }),
-            await userRole.upsert({ name: 'Support Central Manager' }),
-            await userRole.upsert({ name: 'Support Central Worker' }),
+            await userRole.upsert({ 
+                name: 'Admin',
+                defaultCanEdit: true,
+                defaultCanArchive: true,
+                defaultCanActivate: true,
+                defaultCanDelete: true,
+            }),
+            await userRole.upsert({ 
+                name: 'Unassigned',
+                defaultCanEdit: false,
+                defaultCanArchive: false,
+                defaultCanActivate: false,
+                defaultCanDelete: false,
+            }),
+            await userRole.upsert({ 
+                name: 'Maintenance Manager', 
+                defaultCanEdit: true,
+                defaultCanArchive: true,
+                defaultCanActivate: true,
+                defaultCanDelete: false,
+                
+            }),
+            await userRole.upsert({ 
+                name: 'Maintenance Worker', 
+                defaultCanEdit: false,
+                defaultCanArchive: false,
+                defaultCanActivate: false,
+                defaultCanDelete: false,
+             }),
+            await userRole.upsert({ 
+                name: 'IT Manager', 
+                defaultCanEdit: true,
+                defaultCanArchive: true,
+                defaultCanActivate: true,
+                defaultCanDelete: false,
+             }),
+            await userRole.upsert({ 
+            name: 'IT Worker', 
+            defaultCanEdit: false,
+            defaultCanArchive: false,
+            defaultCanActivate: false,
+            defaultCanDelete: false, }),
+            await userRole.upsert({ 
+                name: 'Support Central Manager', 
+                defaultCanEdit: true,
+                defaultCanArchive: true,
+                defaultCanActivate: true,
+                defaultCanDelete: false,
+             }),
+            await userRole.upsert({ 
+                name: 'Support Central Worker',
+                defaultCanEdit: false,
+                defaultCanArchive: false,
+                defaultCanActivate: false,
+                defaultCanDelete: false,
+             }),
         ])
 
         console.log("User Role initialized successfully")
